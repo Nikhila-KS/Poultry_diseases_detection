88f85e45b28106ccafbc8b978afa479888ff21b1
Nikhila-KS <nikhilapersonal122@gmail.com>
2023-12-29 11:20:12 +0530

    added  dialog box for user query

--- Mobile_Application/hen_scan/lib/screens/homeScreen.dart
+++ Mobile_Application/hen_scan/lib/screens/homeScreen.dart
@@ -127,10 +127,41 @@ class homeScreen extends StatelessWidget {
                       Icons.question_answer_rounded,
                       color: Theme.of(context).primaryColorDark,
                     ),
-                    onPressed: () {}),
+                    onPressed: () {
+          showAlertDialog(context);
+        },),
               ],
             ),
           )),
+          
     );
   }
+}
+showAlertDialog(BuildContext context) { 
+  Widget cancelButton = TextButton(
+    child: const Text("Cancel"),
+    onPressed:  () {},
+  );
+  Widget continueButton = TextButton(
+    child: const Text("Continue"),
+    onPressed:  () {},
+  );
+
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: const Text("AlertDialog"),
+    content: const Text("Would you like to continue learning how to use Flutter alerts?"),
+    actions: [
+      cancelButton,
+      continueButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
 }
